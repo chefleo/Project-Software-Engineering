8c9ef1c395c1590b639a80609536c7236612a345
Leonardo Simone Digiorgio <simon.digiorgio@gmail.com>
2018-07-05 01:08:02 +0200

    Fixed spaces in MainActivity

--- webview/app/src/main/java/com/example/simonedigiorgio/webview/MainActivity.java
+++ webview/app/src/main/java/com/example/simonedigiorgio/webview/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.simonedigiorgio.webview;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,27 +17,25 @@ public class MainActivity extends AppCompatActivity {
 	public EditText url;
 
     @Override
-		protected void onCreate(Bundle savedInstanceState) {
-    	 super.onCreate(savedInstanceState);
-    	 setContentView(R.layout.activity_main);
-    	 url = (EditText) findViewById(R.id.Search_url);
-    	 but1 = (Button) findViewById(R.id.button_search);
-	    // Transform the editText for url into a string
-	    //String loadurl = url.getText().toString();
+	protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_main);
+    	url = (EditText) findViewById(R.id.Search_url);
+    	but1 = (Button) findViewById(R.id.button_search);
 
 	    but1.setOnClickListener(new View.OnClickListener() {
     		 @Override
 		     public void onClick(View view) {
-    			 if (url != null) {
-				     // Transform the editText for url into a string
-    				 String loadurl = url.getText().toString();
+    		 	if (url != null) {
+    		 		// Transform the editText for url into a string
+					String loadurl = url.getText().toString();
 
-    				 Intent toy = new Intent(MainActivity.this, second.class);
-				     // Transport the string from the MainActivity to the second_activity
-    				 toy.putExtra("Url", loadurl);
+					Intent toy = new Intent(MainActivity.this, second.class);
+					// Transport the string from the MainActivity to the second_activity
+					toy.putExtra("Url", loadurl);
 
-    				 startActivity(toy);
-    				 } else if (url == null) {
+					startActivity(toy);
+    		 	} else if (url == null) {
 				        //When you do not put a URL, this message does not appear
     				    Toast.makeText(MainActivity.this, "Write url", Toast.LENGTH_SHORT).show();
     			 	}
